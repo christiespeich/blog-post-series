522e39962e04eeb4a192b8dac66d3a3b026e7e5a
christiespeich <christiespeich@users.noreply.github.com>
2015-07-28 09:43:00 -0400

    initial commit

---
+++ blog-post-series.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Plugin Name: Blog Post Series
+Plugin URI:  http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Organizes multiple blog posts into a series
+Version:     0.1
+Author:      Mooberry Dreams
+Author URI:  https://profiles.wordpress.org/mooberrydreams/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: mbd-blog-post-series
+
+Blog Post Series is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+Blog Post Series is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Blog Post Series. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+
+*/
+
+
+if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/cmb2/init.php';
+} elseif ( file_exists( dirname( __FILE__ ) . '/CMB2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/CMB2/init.php';
+}
+
+
+
+	
+	
+	
